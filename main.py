0affc703d7dfd364864dfd94e09212052121c551
kevinkandou <focus.kandou@gmail.com>
2023-12-15 19:00:43 +0800

    get data structure

--- main.py
+++ main.py
@@ -6,27 +6,65 @@ soup = BeautifulSoup(html_doc.text, 'html.parser')
 
 top_stories = soup.find(attrs={'class': 'D9SJMe'})
 sections = top_stories.findAll(attrs={'class': 'PO9Zff Ccj79 kUVvS'})
+base_link = 'https://news.google.com'
+news_dict = {}
+title = ''
+link = ''
+source = ''
+thumbnail = ''
 news_list = []
 
 s = 0
 for s in range(len(sections)):
     sA = sections[s].findAll(attrs={'class': 'XBspb'})
+    sB = sections[s].findAll(attrs={'class': 'IBr9hb'})
+    sC = sections[s].findAll(attrs={'class': 'UwIKyb'})
+
     if(len(sA)>0):
-        '''
-        news_dict= {
-            'title': sA.find('a', 'JtKRv').text
-            # source=sA.findAll(attrs={'class': 'vr1PYe'}),
-            # thumbnail=sA.findAll(attrs={'class': 'Quavad'}),
-            # link=sA.findAll(attrs={'class': 'JtKRv'}).get('href')
+        # Section A
+        title = sA[0].find('a', 'JtKRv').text # title
+        link = str(base_link+sA[0].find('a', 'JtKRv').get('href')[1:]) # link
+        source = sA[0].find('div', 'vr1PYe').text  # source
+        try:
+            thumbnail = sA[0].find(attrs={'class': 'Quavad'}).get('src')  # thumbnail
+        except:
+            thumbnail = 'No Thumbnail'
+        news_dict = {
+            'title' : title,
+            'link' : link,
+            'source' : source,
+            'thumbnail' : thumbnail
         }
         news_list.append(news_dict)
-        '''
-        print(len(sA))
-        print(sA[0].find('a', 'JtKRv').get('href'))
-    '''
-    sectionA.append(sections[s].findAll(attrs={'class': 'XBspb'}))
-    sectionB.append(sections[s].findAll(attrs={'class': 'IBr9hb'}))
-    sectionC.append(sections[s].findAll(attrs={'class': 'f9uzM'}))
-    '''
+    else:
+        # Section B
+        title = sB[0].find('a', 'gPFEn').text  # title
+        link = str(base_link+sB[0].find('a', 'gPFEn').get('href')[1:])  # link
+        source = sB[0].find('div', 'vr1PYe').text  # source
+        try:
+            thumbnail = sB[0].find(attrs={'class': 'Quavad'}).get('src')  # thumbnail
+        except:
+            thumbnail = 'No Thumbnail'
+        news_dict = {
+            'title': title,
+            'link': link,
+            'source': source,
+            'thumbnail': thumbnail
+        }
+        news_list.append(news_dict)
+
+        # Section C
+        for c in range(len(sC)):
+            title = sC[c].find('a', 'gPFEn').text  # title
+            link = str(base_link+sC[c].find('a', 'gPFEn').get('href')[1:])  # link
+            source = sC[c].find('div', 'vr1PYe').text  # source
+            thumbnail = 'No Thumbnail'
+            news_dict = {
+                'title': title,
+                'link': link,
+                'source': source,
+                'thumbnail': thumbnail
+            }
+            news_list.append(news_dict)
 
 print(news_list)
